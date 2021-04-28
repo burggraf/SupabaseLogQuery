be8f0c6c5f70dd22c1e29b4f5e5133166d567dde
burggraf <markb@mantisbible.com>
2021-04-28 05:47:51 -0700

    Update SupabaseLogQuery.sql

--- SupabaseLogQuery.sql
+++ SupabaseLogQuery.sql
@@ -1,10 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS file_fdw;
 CREATE SERVER IF NOT EXISTS logserver FOREIGN DATA WRAPPER file_fdw;
 CREATE FOREIGN TABLE IF NOT EXISTS postgres_log_text
-(line text)
+(log_entry text)
  SERVER logserver OPTIONS (program 'tac pg_log/postgresql.csv', format 'text');
 
 -- 
 -- read the most recent 20 entries from the log file
 --
-SELECT line from postgres_log_text LIMIT 20;
+SELECT log_entry from postgres_log_text LIMIT 20;
