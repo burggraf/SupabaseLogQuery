ac5a0334da9ce786e2b7433d09258f3139450aff
Mark Burggraf <markb@mantisbible.com>
2021-08-28 15:39:39 -0700

    dynamically create postgres_log table based on PostgreSQL 12 or 13

--- 01_setup_postgres_logging.sql
+++ 01_setup_postgres_logging.sql
@@ -1,31 +1,49 @@
-CREATE TABLE IF NOT EXISTS postgres_log
-(
-  log_time text,
-  user_name text,
-  database_name text,
-  process_id text,
-  connection_from text,
-  session_id text,
-  session_line_num text,
-  command_tag text,
-  session_start_time text,
-  virtual_transaction_id text,
-  transaction_id text,
-  error_severity text,
-  sql_state_code text,
-  message text,
-  detail text,
-  hint text,
-  internal_query text,
-  internal_query_pos text,
-  context text,
-  query text,
-  query_pos text,
-  location text,
-  application_name text,
-  extra text,
-  PRIMARY KEY (session_id, session_line_num)
-);
+--DROP TABLE IF EXISTS postgres_log CASCADE;
+--DROP FUNCTION IF EXISTS create_postgres_log_table;
+--DROP FUNCTION IF EXISTS import_postgres_log;
+--DROP FUNCTION IF EXISTS get_postgres_log;
+
+-- create the postgres_log table depending on your version of PostgreSQL
+-- version 13 requires an extra field, version 12 does not
+CREATE OR REPLACE FUNCTION create_postgres_log_table()
+RETURNS VOID AS 
+$$
+DECLARE pg_major_version TEXT;
+BEGIN
+  SELECT substr(version(),12,2) INTO pg_major_version;
+  EXECUTE '
+  CREATE TABLE IF NOT EXISTS postgres_log
+  (
+    log_time text,
+    user_name text,
+    database_name text,
+    process_id text,
+    connection_from text,
+    session_id text,
+    session_line_num text,
+    command_tag text,
+    session_start_time text,
+    virtual_transaction_id text,
+    transaction_id text,
+    error_severity text,
+    sql_state_code text,
+    message text,
+    detail text,
+    hint text,
+    internal_query text,
+    internal_query_pos text,
+    context text,
+    query text,
+    query_pos text,
+    location text,
+    application_name text,'
+    || CASE WHEN pg_major_version < '13' THEN '' ELSE 'extra text,' END || --extra text for pg13, otherwise skip this field for pg12
+    'PRIMARY KEY (session_id, session_line_num)
+  );
+  ';
+END;
+$$ LANGUAGE PLPGSQL;
+select create_postgres_log_table();
 
 CREATE OR REPLACE FUNCTION import_postgres_log()
 RETURNS TEXT AS 
@@ -64,6 +82,7 @@ $$ LANGUAGE PLPGSQL;
 CREATE OR REPLACE FUNCTION get_postgres_log(how_many INTEGER)
 RETURNS SETOF postgres_log AS 
 $$
-SELECT import_postgres_log();
-SELECT * from postgres_log ORDER BY log_time DESC LIMIT how_many;
+  SELECT import_postgres_log();
+  SELECT * from postgres_log ORDER BY log_time DESC LIMIT how_many;
 $$ LANGUAGE SQL;
+
