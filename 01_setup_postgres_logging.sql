cf2189bcdd40e2d422d1fbc7d7d3e28c06cb0da1
Mark Burggraf <markb@mantisbible.com>
2021-08-28 11:08:23 -0700

    fix

--- 01_setup_postgres_logging.sql
+++ 01_setup_postgres_logging.sql
@@ -1,3 +1,32 @@
+CREATE TABLE IF NOT EXISTS postgres_log
+(
+  log_time text,
+  user_name text,
+  database_name text,
+  process_id text,
+  connection_from text,
+  session_id text,
+  session_line_num text,
+  command_tag text,
+  session_start_time text,
+  virtual_transaction_id text,
+  transaction_id text,
+  error_severity text,
+  sql_state_code text,
+  message text,
+  detail text,
+  hint text,
+  internal_query text,
+  internal_query_pos text,
+  context text,
+  query text,
+  query_pos text,
+  location text,
+  application_name text,
+  extra text
+  PRIMARY KEY (session_id, session_line_num)
+);
+
 CREATE OR REPLACE FUNCTION import_postgres_log()
 RETURNS TEXT AS 
 $$
